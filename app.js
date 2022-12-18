2da27ec10d5c821c2e8a621eb1741d16209c1cf7
hayato2022 <gaomusunxiang@gmail.com>
2022-12-18 09:04:35 +0000

    this

--- app.js
+++ app.js
@@ -1,15 +1,5 @@
-// マウスクリックイベントの記述
-// 今回は、クリック後に複数のイベントが行われるようにしたいので、onを付けた書き方
-// 例
-// $('.セレクタ名').on('click', function() {
-//   イベント発生時に行われる処理
-// });
-
 $(function(){
   $('.box1').on('click', function(){
-    $('.box1').addClass('box1-ext');
-  });
-  $('.box1').mouseout(function(){
-    $('.box1').removeClass('box1-ext');
+    $(this).slideUp();
   });
 });
