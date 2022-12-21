bad28b3ed0d585ebf6684d7b93cfedb389f72e3d
hayato2022 <gaomusunxiang@gmail.com>
2022-12-21 07:32:51 +0000

    children

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 $(function(){
-  $('.box1').on('click', function(){
-    $(this).slideUp();
+  // 「button要素がクリックされたときに、処理を実行してください」の意味
+  $('button').on('click', function(){
+    //  .HTML要素直下のすべての子要素を取得する
+    // ここでのchildrenは、「ul要素の子要素」つまり、li要素が対象になる
+    $('ul').children().css('color','red');
   });
 });
